a67371c6b028fb4236244adeaecf3cb8607b4388
Alison Hall <ahall7@sapient.com>
2017-08-21 11:23:17 -0400

    Include Firebase info and packages

---
+++ src/base.js
@@ -0,0 +1,10 @@
+var Rebase = require('re-base');
+
+var base = Rebase.createClass({
+	apiKey: "AIzaSyCsgLq79fYddJzwQeEN8Tryzw91_T47s-c",
+	authDomain: "mock-data-7c200.firebaseapp.com",
+	databaseURL: "https://mock-data-7c200.firebaseio.com",
+	storageBucket: "mock-data-7c200.appspot.com"
+});
+
+export default base;
